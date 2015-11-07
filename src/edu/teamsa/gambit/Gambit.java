288ee7b92b9740e91e38cc80122cd96b96f5ce88
Dandy Taylor <dandy.taylor@curvilinearllc.com>
2015-11-06 20:33:01 -0700

    Initial Commit of Eclipse Project

---
+++ src/edu/teamsa/gambit/Gambit.java
@@ -0,0 +1,26 @@
+/*
+ * Assignment	Group Project
+ * Program		Gambit
+ * Author		Team Something Awesomes
+ * Date			Nov 5, 2015
+ *
+ */
+
+package edu.teamsa.gambit;
+
+/**
+ * This is the main class for the Gambit application.
+ * 
+ * @author Team Something Awesomes
+ *
+ */
+public class Gambit
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/edu/teamsa/gambit/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains all of the classes created by Team Something Awesomes
+ * to support Gambit, a simple blackjack application.
+ * 
+ */
+package edu.teamsa.gambit;
