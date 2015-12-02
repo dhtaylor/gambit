f9bce32b35835e6a1b431e90992fc2353e8cefc1
brae27 <brittanyraehof@hotmail.com>
2015-12-01 18:10:01 -0700

    class hand

---
+++ src/edu/teamsa/gambit/Hand.java
@@ -0,0 +1,21 @@
+package edu.teamsa.gambit;
+
+import java.util.Collection;
+
+public class Hand {
+	
+	public Collection<Card> cards;
+	public int currentValue;
+	public int aceCount;
+	
+	public Hand(){
+		
+	}
+	public void setCurrentValue(int value){
+		//set current value on hand
+	}
+	public int getCurrentValue(){
+		return currentValue;
+	}
+
+}
