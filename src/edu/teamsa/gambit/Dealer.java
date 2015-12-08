5c1391495fbc8e463296ff782ef0462a3e37805e
Dandy Taylor <dandy.taylor@curvilinearllc.com>
2015-12-07 18:52:30 -0700

    Moved some code to Dealer
    Started moving some code to dealer to make the main class easier to
    read. It is starting to get cluttered with stuff that can be refactored
    to functions and methods.

--- src/edu/teamsa/gambit/Dealer.java
+++ src/edu/teamsa/gambit/Dealer.java
@@ -2,4 +2,36 @@ package edu.teamsa.gambit;
 
 public class Dealer extends Player {
 
+	private int limit = 17;
+	private boolean hardLimit = false;
+
+	public Deck getDeck()
+	{
+		Deck deck = new Deck();
+		deck.createDeck();
+		deck.shuffle();
+
+		return deck;
+	}
+	
+	public boolean checkTooFewCards(Deck d)
+	{
+		return (d.size() <= 8) ? true : false;
+	}
+	
+	public int getLimit()
+	{
+		return this.limit;
+	}
+	
+	public boolean isHardLimit()
+	{
+		return this.hardLimit;
+	}
+	
+	public void setHardLimit(boolean b)
+	{
+		this.hardLimit = b;
+	}
+	
 }
